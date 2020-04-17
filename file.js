eda39fc70e149d2b661c67844070da440e041145
pietro <pietro1990@hotmail.it>
2020-04-17 16:17:32 +0200

    esercizio svolto

---
+++ file.js
@@ -0,0 +1,12 @@
+//chiedere all'utente il nome
+var nome_utente = prompt("Qual è il tuo nome?")
+
+//chiedere allutentte il cognome
+var cognome_utente = prompt("Qual è il tuo cognome?")
+
+//chiedere all'utente il colore preferito
+var colore_utente = prompt("Qual è il tuo colore preferito?")
+
+//visualizzare il tutto
+console.log(nome_utente + cognome_utente + colore_utente + "20")
+document.writeln(nome_utente + cognome_utente + colore_utente + "20")
